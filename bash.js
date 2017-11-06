9c7e15c8136ca4e0c762f4651a73b5c5c33b21dc
Shmuel Lotman <shmuel.lotman@gmail.com>
2017-11-06 15:27:30 -0500

    piping initial fix

--- bash.js
+++ bash.js
@@ -6,11 +6,17 @@ process.stdout.write('prompt >')
 process.stdin.on('data', function (data) {
     var cmd = data.toString().trim();
     if(cmd.includes('|')) {
-        var arguments = cmdString.split(/\s*\|\s*/g)
+        var arguments = cmd.split(/\s*\|\s*/g)
+        var func = arguments[0].split(' ')[0],
+        param = arguments[0].split(' ')[1],
+        stdin = arguments.slice(1);
+        commands[func](param, stdin)
+    } else {
+        var arguments = cmd.split(' ').slice(1).join('');
+        cmd = cmd.split(' ')[0]
+        commands[cmd](arguments, null)
     }
-    var arguments = cmd.split(' ').slice(1).join('');
-    cmd = cmd.split(' ')[0]
-    commands[cmd](arguments)
+    
     process.stdout.write(' n\prompt >');
 
 });
--- commands.js
+++ commands.js
@@ -1,77 +1,112 @@
 var fs = require('fs');
 var req = require('request');
 module.exports = {
-    pwd: function() {
-      var output = process.stdout.write(process.cwd().toString());
-      return output;
+    pwd: function(file) {
+      var output = process.cwd().toString();
+      module.exports.done(output)
     },
   
-    date: function() {
+    date: function(file) {
       var date = new Date();
-      var output = process.stdout.write(date.toString());
-      return output;
+      var output = date.toString();
+      module.exports.done(output);
     },
-    ls: function() {
+    ls: function(file) {
         fs.readdir('.', function(err, files) {
+            var finalStr = '';
             if (err) throw err;
             files.forEach(function(file) {
-              process.stdout.write(file.toString() + "\n");
+              finalStr += (file.toString() + "\n");
             })
-            process.stdout.write("prompt > ");
+            module.exports.done(finalStr)
           });
     },
     echo: function(file) {
-        console.log(file);
+        module.exports.done(file);
+        
     },
-    cat: function(file) {
+    cat: function(file, stdin = null) {
+        var finalStr = '';
         fs.readFile(file, function(err, data) {
             if(err) throw err;
-            console.log(data.toString())
-            console.log("prompt > ");
+            finalStr += (data.toString())
+            if(stdin === null) {
+                module.exports.done(finalStr, null)
+            } else {
+                if(stdin.length === 0) {
+                    module.exports.done(finalStr, null)
+                }
+                module.exports.done(finalStr, stdin)
+            }
         })
         
     },
-    head: function(file) {
+    head: function(file, stdin = null) {
         fs.readFile(file, function(err, data) {
             if(err) throw err;
-            var lines = data.toString().trim().split('\n');
+            var lines = data.toString().trim().split('\n'),
+            finalStr = '';
             for(var i = 0; i < 5; i++) {
-                console.log(lines[i] + '\n')
+                finalStr += (lines[i] + '\n')
             }
-            console.log("prompt > ");
+            if(stdin === null) {
+                module.exports.done(finalStr)
+            } else {
+                if(stdin.length === 0) {
+                    module.exports.done(finalStr, null)
+                }
+                module.exports.done(finalStr, stdin)
+            }
         })
     },
-    tail: function(file) {
+    tail: function(file, stdin = null) {
         fs.readFile(file, function(err, data) {
             if(err) throw err;
-            var lines = data.toString().trim().split('\n');
+            var lines = data.toString().trim().split('\n'),
+            finalStr = '';
             for(var i = lines.length-5; i < lines.length; i++) {
-                console.log(lines[i] + '\n')
+                finalStr += (lines[i] + '\n')
             }
-            console.log("prompt > ");
+            if(stdin === null) {
+                module.exports.done(finalStr, null)
+            } else {
+                module.exports.done(file, stdin)
+            }
         })
     },
-    sort: function(file) {
+    sort: function(file, stdin=null) {
         fs.readFile(file, function(err, data) {
             if(err) throw err;
-            var lines = data.toString().trim().split('\n');
+            var lines = data.toString().trim().split('\n'),
+            finalStr = '';
             lines = lines.sort();
             lines.forEach(function(a) {
-                console.log(a + '\n')
+                finalStr += (a + '\n')
             })
-            console.log("prompt > ");
+            if(stdin === null) {
+                module.exports.done(finalStr, null)
+            } else {
+                module.exports.done(file, stdin)
+            }
         })
     }, 
-    wc: function(file) {
-        fs.readFile(file, function(err, data) {
-            if(err) throw err;
-            var lines = data.toString().trim().split('\n');
-            console.log(lines.length)
-            console.log("prompt > ");
-        })
+    wc: function(file, stdin=null) {
+        if(stdin === null || stdin.length === 0) {
+            var lengthOfLines = file.split('\n').length;
+            module.exports.done(lengthOfLines, null)
+        } else {
+            fs.readFile(file, function(err, data) {
+                if(err) throw err;
+                var lines = data.toString().trim().split('\n');
+                let lengthOfLines = lines.length;
+                module.exports.done(lengthOfLines, stdin)
+            })
+            
+        }
+        
         
     },
-    uniq: function(file) {
+    uniq: function(file, stdin=null) {
         fs.readFile(file, function(err, data) {
             if(err) throw err;
             var lines = data.toString().trim().split('\n').sort();
@@ -81,15 +116,37 @@ module.exports = {
                     final += (lines[i] + '\n')
                 }
             }
-            console.log(final)
-            console.log("prompt > ");
+            if(stdin === null) {
+                module.exports.done(final, null)
+            } else {
+                module.exports.done(file, stdin)
+            }
         })
     },
-    curl: function(file) {
+    curl: function(file, stdin=null) {
+        var finalStr = '';
         req(file.toString(), function (error, response, body) {
             console.log('error:', error); // Print the error if one occurred
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            console.log('body:', body); // Print the HTML for the url.
+            finalStr += body;
+            module.exports.done(finalStr)
           });
+    },
+    done: function(output, stdin) {
+        if(stdin === null) {
+            console.log(output)
+        } else {
+            var expts = module.exports;
+            var x = stdin.shift();
+            if(typeof x !== 'undefined') {
+                expts[x](output, stdin)
+            } else {
+                console.log(output)
+            }
+            
+            
+        }
+        
+        console.log('prompt >')
     }
 }
